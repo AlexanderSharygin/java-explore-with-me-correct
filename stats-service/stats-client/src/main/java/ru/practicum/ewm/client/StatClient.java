d62ec03b6893db588d82327ea8da6fdfef9df94e
=AlexSharygin <AlexSharygin1989@yandex.ru>
2023-03-12 17:07:07 +0300

    FixesForLocalDebuging

--- stats-service/stats-client/src/main/java/ru/practicum/ewm/client/StatClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/ewm/client/StatClient.java
@@ -18,7 +18,7 @@ public class StatClient extends BaseClient {
     private static final String API_PREFIX = "";
 
     @Autowired
-    public StatClient(@Value("http://stats-server:9090") String serverUrl, RestTemplateBuilder builder) {
+    public StatClient(@Value("http://localhost:9090") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
